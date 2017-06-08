158b0b58d522c3939d52ee687be531fbe193a09b
JuanP <jpgarciajimenez@gmail.com>
2017-06-08 23:18:19 +0200

    Fixed class

--- view/header.php
+++ view/header.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <?php header('Content-Type: text/html; charset=UTF-8');
-mb_internal_encoding('UTF-8');
  
 // Esto le dice a PHP que generaremos cadenas UTF-8
-mb_http_output('UTF-8');
 ?>
 <html>
     <head>
